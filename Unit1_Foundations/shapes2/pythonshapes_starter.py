fc30295168ec0265b5fa7e397945727fafdd8432
melodyzheng <zhengmelody99@gmail.com>
2017-07-06 12:37:29 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -1,3 +1,4 @@
+
 from turtle import *
 import math
 
@@ -5,17 +6,25 @@ import math
 t = Turtle()
 
 # Set Up your screen and starting position.
+color = input("What color?")
+
+pencolor(color)
+
 setup(500,300)
-x_pos = -250
-y_pos = -150
+x_pos = 0
+y_pos = 0
 t.setposition(x_pos, y_pos)
+x= 0
+begin_fill()
+s = input("How many sides?")
+for x in range (s):
+    forward(50)
+    right(360/int(s))
+    x = x+1
+
+fillcolor(color)
+end_fill()
+
 
 ### Write your code below:
 
-
-
-
-
-
-# Close window on click.
-exitonclick()
