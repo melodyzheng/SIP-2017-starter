cc61962fbe9f9ed1105645956d70eb124e328d1d
melodyzheng <zhengmelody99@gmail.com>
2017-07-10 15:57:37 -0700

    Update obamicon_starter.py

--- Unit1_Foundations/obamicon/obamicon_starter.py
+++ Unit1_Foundations/obamicon/obamicon_starter.py
@@ -6,16 +6,26 @@ red = (217, 26, 33)
 lightBlue = (112, 150, 158)
 yellow = (252, 227, 166)
 
+color1 = input("What color?")
+
 # Import image.
-my_image = Image.open("IMAGENAME") #change IMAGENAME to the path on your computer to the image you're using
+my_image = Image.open("brooklyn.jpg") #change IMAGENAME to the path on your computer to the image you're using
 image_list = my_image.getdata() #each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
 image_list = list(image_list) #Turns the sequence above into a list. The list can be iterated through in a loop.
 
 recolored = [] #list that will hold the pixel data for the new image.
 
 #YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
-
-
+for pixel in image_list:
+    intensity = pixel[0] + pixel[1] +pixel[2]
+    if intensity <= 182:
+        recolored.append(darkBlue)
+    if intensity > 182 and intensity <= 364:
+        recolored.append(red)
+    if intensity > 364 and intensity <= 546:
+        recolored.append(lightBlue)
+    if intensity > 546:
+        recolored.append(yellow)
 
 # Create a new image using the recolored list. Display and save the image.
 new_image = Image.new("RGB", my_image.size) #Creates a new image that will be the same size as the original image.
